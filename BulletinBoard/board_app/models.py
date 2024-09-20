a53840bb1e4b3a0c0f2e9ac5015082b8d2bf040d
Tasank <apanasovich04@mail.ru>
2024-09-20 20:24:14 +0500

    Initial commit

---
+++ BulletinBoard/board_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Класс пользователя
+class User(models.Model):
+    pass
+# Класс уведомления
+class Notifications(models.Model):
+    pass
+# Класс отклика
+class Response(models.Model):
+    pass
+# Класс объявления
+class Announce(models.Model):
+    pass
+# Класс категории
+class Category(models.Model):
+    pass
