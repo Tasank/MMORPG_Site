f23df60721392e626355072fdd06780722ae35ef
Tasank <apanasovich04@mail.ru>
2024-09-27 20:52:17 +0500

    Initial commit

--- BulletinBoard/board_app/admin.py
+++ BulletinBoard/board_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Response
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'created_ad')
+    search_fields = ('title', 'text')
+    list_filter = ('category',)
+    date_hierarchy = 'created_ad'
+    actions = ['approve_posts']
+
+    def approve_posts(self, request, queryset):
+        queryset.update(status=True)
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ('post', 'user', 'status', 'created_ad')
+    search_fields = ('text',)
+    list_filter = ('status',)
+    date_hierarchy = 'created_ad'
