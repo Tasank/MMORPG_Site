c8547ff2733ad03c311f123cafbfa27c94f2ab3d
Tasank <apanasovich04@mail.ru>
2024-10-07 20:15:48 +0500

    07.10.2024 обновление

--- BulletinBoard/board_app/urls.py
+++ BulletinBoard/board_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostList, CreatePost, PostDetail
+from .views import PostList, CreatePost, PostDetail, EditPost, DeletePost
 
 urlpatterns = [
     path('', PostList.as_view(), name='home'),
     path('post/<int:post_id>/', PostDetail.as_view(), name='detail_post'),
     path('created_post/', CreatePost.as_view(), name='created_post'),
+    path('post/edit/<int:pk>/', EditPost.as_view(), name='edit_post'),
+    path('post/delete/<int:pk>/', DeletePost.as_view(), name='delete_post'),
 ]
